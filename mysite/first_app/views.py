a418bda7601ada6251114be3555c4bd634a89963
olegonyxov <olegonyxov@gmail.com>
2022-01-13 05:47:01 +0200

    top100

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -16,7 +16,8 @@ def movies_all(request):
 def movies_top_ten(request):
     get_top = Movie.objects.order_by('-user_rating')[:10]
     return render(request, "movies_all.html", context={'get_all': get_top})
-def movies_top_input(request,top_input=10):
-    get_count = Movie.objects.order_by('-user_rating')[:top_input]
-    return render(request, "movies_all.html", context={'get_all': get_count})
+def movies_top_input(request,top_input=1):
+     get_count = Movie.objects.order_by('-user_rating')[:top_input]
+     return render(request, "movies_all.html", context={'get_all': get_count})
+
 
