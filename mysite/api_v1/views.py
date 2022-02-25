75c101db6bf2ca8a2c8497a05c2a18bc0485b28f
olegonyxov <olegonyxov@gmail.com>
2022-02-25 22:52:06 +0200

    api-1

---
+++ mysite/api_v1/views.py
@@ -0,0 +1,23 @@
+from rest_framework import authentication, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from mysite.first_app.models import Movie
+
+
+class ListMovie(APIView):
+    """
+    View to list all movies in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all movies.
+        """
+        movies = [movie.title for movie in Movie.objects.all()]
+        return Response(movies)
