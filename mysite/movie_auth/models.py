9ad8e3dacc99efcf011e91ebfb54b414ce4db8a3
olegonyxov <olegonyxov@gmail.com>
2022-03-18 11:35:48 +0200

    req_timer

--- mysite/movie_auth/models.py
+++ mysite/movie_auth/models.py
@@ -1,10 +1,11 @@
-from django.db import models
-from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser )
+from django.contrib.auth.models import (BaseUserManager)
 from django.contrib.auth.models import User
-#
-class User(User):
-#     # user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
-    dob= models.DateField()
+from django.db import models
+
+
+class User(User):  # strange but needed
+    # user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    dob = models.DateField()
 
 
 class M_UserManager(BaseUserManager):
@@ -13,7 +14,7 @@ class M_UserManager(BaseUserManager):
             raise ValueError("Please enter Your email")
         if User.objects.filte(email=email).exists():
             raise ValueError("Email already used")
-        user =self.model(
+        user = self.model(
             username=username,
             email=email,
             dob=dob
