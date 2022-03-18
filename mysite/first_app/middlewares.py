9aa1a5ae02aa758a016bd0ce6ef051ce9a9dbcd3
olegonyxov <olegonyxov@gmail.com>
2022-03-18 11:31:21 +0200

    req_timer

---
+++ mysite/first_app/middlewares.py
@@ -0,0 +1,12 @@
+import time
+
+
+class ReqTimer:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        time_start = time.monotonic()
+        response = self.get_response(request)
+        response["X-Request-Timing"] = str(time.monotonic() - time_start)[0:5]
+        return response
