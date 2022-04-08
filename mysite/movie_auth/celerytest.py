ee38346b4e7548b73a9c5fc1684ca2e75833ff96
olegonyxov <olegonyxov@gmail.com>
2022-04-08 04:27:42 +0300

    +test

--- mysite/movie_auth/celerytest.py
+++ mysite/movie_auth/celerytest.py
@@ -8,8 +8,7 @@ class CeleryTest(unittest.TestCase):
         my_queryset= Mock(User.objects.all()[:10])
         my_queryset.update(is_notified=False)
         result=change_flag(my_queryset)
-        expected_result = 0
-        self.assertEqual(expected_result, result)
+        self.assertEqual(0, result)
 
 
 
