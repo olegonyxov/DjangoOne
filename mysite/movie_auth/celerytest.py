99ab4e405e8cba3c89f8f97523a2f3c0dd366c1a
olegonyxov <olegonyxov@gmail.com>
2022-04-08 04:38:04 +0300

    end

--- mysite/movie_auth/celerytest.py
+++ mysite/movie_auth/celerytest.py
@@ -5,7 +5,7 @@ from tasks import change_flag
 
 class CeleryTest(unittest.TestCase):
     def test_change_flag(self):
-        my_queryset= Mock(User.objects.all()[:10])
+        my_queryset= Mock(User.objects.all()[:10])          #mb try a simple deepcopy
         my_queryset.update(is_notified=False)
         result=change_flag(my_queryset)
         self.assertEqual(0, result)
